49b6ba0c4c892affb2ffd272e000a9c3000f89e3
Anis114 <93031600+Anis114@users.noreply.github.com>
2022-08-01 11:16:43 +0100

    aaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from 'react';
 
+
+class App extends React.Component {
+state={
+  person:{fullName:"***",
+          bio:"***",
+          imgSrc:'***',
+          profession:'***'},
+  shows:true,
+  date: new Date()
+   }
+   toggle=()=>
+    {this.setState({shows:!this.state.shows})
+    }
+
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+     
+
+
+      
+    }
+
+    
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  
+  render(){
+
+return( <div>
+{
+this.state.shows  &&  <div><h1>{this.state.person.fullName}</h1>
+<h2>{this.state.person.bio}</h2>
+<h3>{this.state.person.imgSrc}</h3>
+<h4>{this.state.person.profession}</h4>
+<h2>It is {this.state.date.getSeconds-}</h2>
+</div>}
+<button onClick={(e)=>this.toggle() }>SHOW STATE</button>
+</div>);
+  }
+};
 export default App;
+
+ 
